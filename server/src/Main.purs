903e66f39af5981e8962a71d95021cd8dc85201a
Ryan L. Bell <ryan.l.bell@gmail.com>
2015-03-29 19:23:22 -0400

    Added a server for serving hangman and puzzles

---
+++ server/src/Main.purs
@@ -0,0 +1,36 @@
+module Main where
+
+import Control.Monad.Eff
+
+import Node.Express.Types
+import Node.Express.App
+import Node.Express.Handler
+
+handler :: Handler
+handler = sendJson { puzzle: "This came from the server" }
+
+app :: App
+app = do
+  get "/puzzle" handler
+
+attach :: Application -> ExpressM Unit
+attach = apply app
+
+foreign import foreignMain
+  """
+  function foreignMain(attachFn) {
+    return function() {
+      var express = require('express');
+      var app     = express();
+      app.use('/hangman', express.static('../client'));
+      attachFn(app)();
+      var server = app.listen(3000, function() {
+        var port = server.address().port
+
+        console.log('Hangman listening on port %s', port)
+      });
+    }
+  }
+  """ :: forall e. (Application -> ExpressM Unit) -> Eff e Unit
+
+main = foreignMain attach
